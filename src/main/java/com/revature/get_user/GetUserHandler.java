3ab724c7c15656e0f8e7bec13f9fedc2ea25507d
robertni <robertni@outlook.fr>
2021-09-27 12:28:26 -0400

    refactored files

--- src/main/java/com/revature/get_user/GetUserHandler.java
+++ src/main/java/com/revature/get_user/GetUserHandler.java
@@ -7,7 +7,6 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.revature.repos.UserRepository;
 
 public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
--- src/main/java/com/revature/get_user/User.java
+++ src/main/java/com/revature/get_user/User.java
@@ -1,4 +1,4 @@
-package com.revature.models;
+package com.revature.get_user;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
